fc740f7c2fb2269fac683e7365b81913582e515d
Keijiro Takahashi <keijiro@gmail.com>
2021-03-02 18:00:42 +0900

    Make fragments pre-deterministic

--- Assets/Script/Fragment.cs
+++ Assets/Script/Fragment.cs
@@ -1,5 +1,4 @@
 using Unity.Burst;
-using Unity.Collections;
 using Unity.Mathematics;
 using Unity.Jobs;
 using UnityEngine;
@@ -8,6 +7,9 @@ using Klak.Math;
 
 namespace DxrCrystal {
 
+//
+// Fragment animation with pre-deterministic Newtonian motion
+//
 readonly struct Fragment
 {
     #region Configuration structure
@@ -16,13 +18,15 @@ readonly struct Fragment
     public struct Config
     {
         [SerializeField] float _radius;
-        [SerializeField] float _speed;
+        [SerializeField] float _offset;
         [SerializeField] float _spin;
+        [SerializeField] float _drag;
         [SerializeField] float _scale;
 
         public float Radius => _radius;
-        public float Speed => _speed;
+        public float Offset => _offset;
         public float Spin => _spin;
+        public float Drag => _drag;
         public float Scale => _scale;
     }
 
@@ -30,98 +34,84 @@ readonly struct Fragment
 
     #region Data storage
 
+    readonly Config _config;
     readonly XXHash _hash;
-    readonly float3 _position;
-    readonly quaternion _rotation;
+    readonly float _time;
 
     #endregion
 
-    #region Public accessors
+    #region Constructor
 
-    public float3 Position => _position;
-    public quaternion Rotation => _rotation;
-
-    public float3 GetScale(in Config config)
-      => math.float3(1, 1, 1) * (config.Scale * _hash.Float(0.3f, 1.0f, 6));
+    public Fragment(in Config config, uint seed, float time)
+    {
+        _config = config;
+        _hash = new XXHash(seed);
+        _time = time;
+    }
 
     #endregion
 
-    #region Private properties and methods
+    #region Properties
 
-    static float3 GetInitialPosition(in XXHash hash, in Config config)
-      => hash.InSphere(1) * config.Radius;
+    // Current state
 
-    static quaternion GetInitialRotation(in XXHash hash)
-      => hash.Rotation(2);
+    public float3 Position
+      => -TotalMotion * (math.exp(-_config.Drag * _time) - 1) + InitialPosition;
 
-    float3 GetVelocity(in Config config)
-      => math.normalize(GetInitialPosition(_hash, config)) *
-           _hash.Float(0.1f, 1.0f, 3) * config.Speed;
+    public float Spin
+      => -TotalSpin * (math.exp(-_config.Drag * _time) - 1);
 
-    float3 GetDeltaPosition(in Config config, float dt)
-      => GetVelocity(config) * dt;
+    public quaternion Rotation
+      => math.mul(InitialRotation, quaternion.AxisAngle(SpinAxis, Spin));
 
-    float3 RotationAxis
+    public float3 Scale
+      => math.float3(1, 1, 1) * (_config.Scale * _hash.Float(0.2f, 1.0f, 1));
+
+    // Initial state
+
+    float3 InitialPosition
+      => TotalMotion * (_config.Offset * _hash.Float(0.2f, 1.0f, 2));
+
+    quaternion InitialRotation
+      => _hash.Rotation(3);
+
+    float3 SpinAxis
       => _hash.Direction(4);
 
-    float GetAngularVelocity(in Config config)
-      => _hash.Float(5) * config.Spin;
+    // Total amount of motion
 
-    quaternion GetDeltaRotation(in Config config, float dt)
-      => quaternion.AxisAngle(RotationAxis, GetAngularVelocity(config) * dt);
+    float3 TotalMotion
+      => _hash.InSphere(5) * _config.Radius;
 
-    #endregion
-
-    #region Factory methods and constructor
-
-    public static Fragment InitialState(uint seed, in Config config)
-    {
-        var hash = new XXHash(seed);
-        return new Fragment(hash,
-                            GetInitialPosition(hash, config),
-                            GetInitialRotation(hash));
-    }
-
-    public Fragment(in XXHash hash, float3 position, quaternion rotation)
-    {
-        _hash = hash;
-        _position = position;
-        _rotation = rotation;
-    }
-
-    #endregion
-
-    #region Frame advance method
-
-    public Fragment NextFrame(in Config config, float dt)
-      => new Fragment(_hash,
-                      _position + GetDeltaPosition(config, dt),
-                      math.mul(_rotation, GetDeltaRotation(config, dt)));
+    float TotalSpin
+      => _hash.Float(6) * _config.Spin;
 
     #endregion
 }
 
+//
+// Fragment transform update job
+//
 [BurstCompile]
 struct FragmentUpdateJob : IJobParallelForTransform
 {
-    NativeArray<Fragment> _rocks;
     Fragment.Config _config;
-    float _dt;
+    XXHash _hash;
+    float _time;
 
-    public FragmentUpdateJob(NativeArray<Fragment> rocks, in Fragment.Config config, float dt)
+    public FragmentUpdateJob(in Fragment.Config config, uint seed, float time)
     {
-        _rocks = rocks;
         _config = config;
-        _dt = dt;
+        _hash = new XXHash(seed);
+        _time = time;
     }
 
     public void Execute(int index, TransformAccess xform)
     {
-        var rock = _rocks[index].NextFrame(_config, _dt);
-        xform.localPosition = rock.Position;
-        xform.localRotation = rock.Rotation;
-        xform.localScale = rock.GetScale(_config);
-        _rocks[index] = rock;
+        var frag = new Fragment(_config, _hash.UInt((uint)index), _time);
+        xform.localPosition = frag.Position;
+        xform.localRotation = frag.Rotation;
+        xform.localScale = frag.Scale;
     }
 }
 
--- Assets/Script/FragmentGroup.cs
+++ Assets/Script/FragmentGroup.cs
@@ -1,7 +1,5 @@
-using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Jobs;
-using Klak.Math;
 
 namespace DxrCrystal {
 
@@ -14,13 +12,10 @@ sealed class FragmentGroup : MonoBehaviour
     [SerializeField] Material _reflectiveMaterial;
     [SerializeField] Material _emissiveMaterial;
 
-    NativeArray<Fragment> _frags;
     TransformAccessArray _taa;
 
     void Start()
     {
-        var hash = new XXHash(_randomSeed);
-        var frags = new Fragment[_instanceCount];
         var xforms = new Transform[_instanceCount];
 
         for (var i = 0u; i < _instanceCount; i++)
@@ -29,23 +24,18 @@ sealed class FragmentGroup : MonoBehaviour
             var go = ObjectFactory.CreateDoubleMeshObject
               ("Fragment", mesh, _reflectiveMaterial, _emissiveMaterial);
 
-            frags[i] = Fragment.InitialState(hash.UInt(i), _fragmentConfig);
             xforms[i] = go.transform;
         }
 
-        _frags = new NativeArray<Fragment>(frags, Allocator.Persistent);
         _taa = new TransformAccessArray(xforms);
     }
 
     void Update()
-      => new FragmentUpdateJob(_frags, _fragmentConfig, Time.deltaTime)
+      => new FragmentUpdateJob(_fragmentConfig, _randomSeed, Time.time)
            .Schedule(_taa).Complete();
 
     void OnDestroy()
-    {
-        _frags.Dispose();
-        _taa.Dispose();
-    }
+      => _taa.Dispose();
 }
 
 } // namespace DxrCrystal
